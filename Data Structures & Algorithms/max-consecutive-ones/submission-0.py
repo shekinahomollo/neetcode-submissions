382e54a8e2ebaff3ee062583e2df555ce5f12725
Nicole Shekinah <shekina.n.omolo@gmail.com>
2026-08-24 12:17:15 +0300

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        max_count = 0
+        count = 0
+        for num in nums:
+            if num == 1:
+                count += 1
+            else:
+                count = 0
+            max_count = max(count, max_count)
+
+        return max_count
