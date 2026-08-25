46635acff6734827ce906f4a421088e0317bc90a
Nicole Shekinah <shekina.n.omolo@gmail.com>
2026-08-25 20:35:36 +0300

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        res = 0
+        for num in nums:
+            res += count[num]
+            count[num] += 1
+        return res
